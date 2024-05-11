7266cd8866d5cc46f9a00e41110df2670a11696d
khalid aqrabawi <k-0-1@outlook.com>
2024-05-11 14:54:19 +0300

    changes

--- Controllers/DashboardsController.cs
+++ Controllers/DashboardsController.cs
@@ -2,23 +2,33 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AspnetCoreMvcFull.Models;
 using System.Security.Claims;
+using RecepieBlog.ViewsDataModel;
+using AspnetCoreMvcFull.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspnetCoreMvcFull.Controllers;
 
 public class DashboardsController : Controller
 {
   private readonly IHttpContextAccessor _httpContextAccessor;
+  private readonly ApplicationDbContext _context;
 
-  public DashboardsController(IHttpContextAccessor httpContextAccessor)
+  public DashboardsController(IHttpContextAccessor httpContextAccessor, ApplicationDbContext context)
   {
     _httpContextAccessor = httpContextAccessor;
+    _context = context;
   }
 
-  public IActionResult Index()
+  public async Task<IActionResult> Index()
   {
+    var model = new DashboardModel();
     string userId = HttpContext.Session.GetString(ClaimTypes.NameIdentifier);
     ViewBag.FullName = HttpContext.Session.GetString(ClaimTypes.Name) + " " + HttpContext.Session.GetString(ClaimTypes.Surname);
     ViewBag.RoleName = HttpContext.Session.GetString(ClaimTypes.Actor);
-    return View();
+    model.UsersCount = await _context.Users.CountAsync();
+    model.ChefsCount = await _context.Chefs.CountAsync();
+    model.RecepiesCount = await _context.Recepies.CountAsync();
+    model.CategoriesCount = await _context.Categorys.CountAsync();
+    return View(model);
   }
 }
---
+++ ViewsDataModel/DashboardModel.cs
@@ -0,0 +1,10 @@
+namespace RecepieBlog.ViewsDataModel
+{
+  public class DashboardModel
+  {
+    public int UsersCount { get; set; }
+    public int ChefsCount { get; set; }
+    public int RecepiesCount { get; set; }
+    public int CategoriesCount { get; set; }
+  }
+}
